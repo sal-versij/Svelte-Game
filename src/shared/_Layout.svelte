7515823f96c97b6a2c588ee061f2690305fb59ae
sal-versij <salvoluca1212000@gmail.com>
2022-02-06 19:50:31 +0100

    Preparazione layout e framework/librerie
    Signed-off-by: sal-versij <salvoluca1212000@gmail.com>

---
+++ src/shared/_Layout.svelte
@@ -0,0 +1,28 @@
+<header/>
+<main>
+    <slot/>
+</main>
+<footer/>
+
+<style global lang="scss">
+  html, body {
+    margin: 0;
+    min-height: 100%;
+  }
+
+  body {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+
+    header {
+    }
+
+    main {
+      flex-grow: 1;
+    }
+
+    footer {
+    }
+  }
+</style>
